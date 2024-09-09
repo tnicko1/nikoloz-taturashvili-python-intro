742280408a179f5fd2ca1d7ab1172b19d81d8249
Nikoloz Taturashvili <tnicko@proton.me>
2024-09-10 01:34:26 +0400

    task 4 completed

---
+++ Homework-1/task_4.py
@@ -0,0 +1,4 @@
+print("Celsius to Fahrenheit calculator")
+celsiusValue = int(input("Temperature in Celsius: "))
+fahrenheitValue = (celsiusValue * 9 / 5) + 32
+print("Temperature in Fahrenheit:",fahrenheitValue)
