1d40edc4ba3b0b0e8601d53679b74f95011ad307
Nikoloz Taturashvili <tnicko@proton.me>
2024-09-13 00:01:24 +0400

    task 2 complete

---
+++ Homework-2/task_2.py
@@ -0,0 +1,13 @@
+#I wanted to add input validation as well, but it seems too advanced for now
+#So I will assume that user correctly enters the integer value
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+
+if number_1 == 0 or number_2 == 0:
+    print("Neither number can be zero")
+    exit(1)
+
+if number_1 % number_2 == 0:
+    print(f"{number_1} is multiple of {number_2}")
+else:
+    print(f"{number_1} is not multiple of {number_2}")
