9037eeb193bc374c847321bc8a7f86e7240951bb
Nikoloz Taturashvili <tnicko@proton.me>
2024-09-20 23:42:14 +0400

    introduced math module

--- Homework-3/task_1.py
+++ Homework-3/task_1.py
@@ -1,5 +1,7 @@
+from math import pow
+
 print("X^Y")
 x = int(input("Please input X: "))
 print(f"{x}^Y")
 y = int(input("Please input Y: "))
-print(f"{x}^{y} = {x ** y}")
+print(f"{x}^{y} = {pow(x, y)}")
