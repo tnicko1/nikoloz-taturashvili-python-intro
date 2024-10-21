699c842741affb811e277156f98123cdb46b6d28
Nikoloz Taturashvili <tnicko@proton.me>
2024-10-22 00:46:05 +0400

    task 2 updated

--- Homework-7/task_2.py
+++ Homework-7/task_2.py
@@ -1,6 +1,5 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
 user_input = input("Enter a string: ")
 for i in range(len(user_input)):
-    if user_input[i] in vowels:
+    if user_input[i] in "aeiouAEIOU":
         continue
     print(user_input[i], end="")
