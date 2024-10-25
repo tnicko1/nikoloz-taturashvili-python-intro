d5fde16dbc3779abd9fd0a845869e4960a5ca30c
Nikoloz Taturashvili <tnicko@proton.me>
2024-10-25 23:31:27 +0400

    task 5

---
+++ Homework-10/task_5.py
@@ -0,0 +1,9 @@
+def inverted_text(text: str) -> str:
+    return text[::-1]
+
+# Test
+print(inverted_text("hello"))  # Output: olleh
+print(inverted_text("world"))  # Output: dlrow
+print(inverted_text("Python"))  # Output: nohtyP
+print(inverted_text("Programming"))  # Output: gnimmargorP
+print(inverted_text("Language"))  # Output: egaugnaL
