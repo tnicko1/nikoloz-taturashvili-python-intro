bdd795f2d6929f90c69e970a1fd2b1b69f22cf25
Nikoloz Taturashvili <tnicko@proton.me>
2024-11-11 20:54:47 +0400

    task 1

---
+++ Midterm/task_1.py
@@ -0,0 +1,6 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+print(f"Hello {first_name} {last_name}.\nAge: {age}.\nCity: {city}.")
